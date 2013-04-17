0087f8c1480129e8a82669c84008fef97d811a58
Valentine Zavadsky <saicheg@gmail.com>
2013-04-17 23:44:31 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CcnaGoogle::Application.config.secret_token = 'b3b94c044fe861e0328a195b333e9b7144d4f1c3afae8c9f3e36ebcfb7b3dfaf62bcb11cbd0a0137021aa36cb0db7bf21ca3d6df22d2dcaeba11feb43363bab1'
