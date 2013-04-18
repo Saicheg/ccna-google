477747daffc52a9dd89aec194454241f0a99fc3a
Valentine Zavadsky <saicheg@gmail.com>
2013-04-18 15:10:17 +0300

    12

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ URL2 = 'https://dl.dropboxusercontent.com/u/12488517/ccna.html'
 
 doc = Nokogiri::HTML(open(URL1))
 doc.css('.post-318 p').each do |p|
+  puts p.text
+
   text = p.css('strong').text.gsub(/\A[^a-zA-z]*/, '')
   all = p.text.gsub(/\A[^a-zA-z]*/, '')
   answers = all.split("\n").reject{|t| t == text}
   right_answers = p.css('span').map{|el| el.text}
-
-  puts answers
 
   question = Question.create(text: text)
   answers.each do |answer_text|
@@ -27,12 +27,9 @@ end
 
 doc = Nokogiri::HTML(open(URL2))
 doc.css('#post-body-513074003750928526 p').each_slice(2) do |data|
-  text = data.first.text.gsub("\n",'')
+  text = data.first.text
   answers = data.last.text.split("\n").map(&:strip)
   right_answers = data.last.css('strong').map{|x| x.text.strip}.flat_map{|x| x.split("\n")}
-
-  puts text
-  puts answers
 
   question = Question.create(text: text)
   answers.each do |answer_text|
